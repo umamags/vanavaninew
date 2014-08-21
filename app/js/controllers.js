cc2d535d97d16d70a1265cec20825c1e9ada9487
Mahesh Natarajan <umamags@gmail.com>
2014-08-20 22:25:37 -0400

    homepage changes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,83 +3,85 @@
 /* Controllers */
 var vanavaniControllers = angular.module('vanavaniControllers', []);
 
-vanavaniControllers.controller('teachersListCtrl', ['$scope', 'Teacher',
-  function($scope, Teacher) {
-    $scope.teachers = Teacher.query();
-    $scope.orderProp = 'sno';
-  }]);
+vanavaniControllers.controller('teachersListCtrl', [ '$scope', 'Teacher',
+		function($scope, Teacher) {
+			$scope.teachers = Teacher.query();
+			$scope.orderProp = 'sno';
+		} ]);
 
+vanavaniControllers.controller('photoShowAllCtrl', [ '$scope', '$routeParams',
+		'Photo', function($scope, $routeParams, Photo) {
+			$scope.photos = Photo.query({
+				folderName : $routeParams.folderName
+			}, function(photo) {
+			});
+		} ]);
 
-vanavaniControllers.controller('photoShowAllCtrl', ['$scope', '$routeParams', 'Photo',
-     function($scope, $routeParams, Photo) {
-       $scope.photos = Photo.query({folderName: $routeParams.folderName}, function(photo) {
-       });
-     }]);
+vanavaniControllers.controller('photoDetailCtrl', [ '$scope', '$routeParams',
+		function($scope, $routeParams) {
+			var name = $routeParams.imageName;
+			name = name.replace("_medium.jpg", "");
+			name = name.replace("_medium.JPG", "");
 
-vanavaniControllers.controller('photoDetailCtrl', ['$scope', '$routeParams',
-       function($scope, $routeParams) {
-		  var name = $routeParams.imageName;
-	      name = name.replace("_medium.jpg", "");
-		  name = name.replace("_medium.JPG", "");	    
-	
-		  $scope.folderName = $routeParams.folderName;
-		  $scope.imageName = name;
-       }]);
+			$scope.folderName = $routeParams.folderName;
+			$scope.imageName = name;
+		} ]);
 
-vanavaniControllers.controller('photoTreeCtrl', ['$scope', '$routeParams',
-       function($scope, $routeParams) {
-       }]);
+vanavaniControllers.controller('photoTreeCtrl', [ '$scope', '$routeParams',
+		function($scope, $routeParams) {
+		} ]);
 
-vanavaniControllers.controller('mentorsCtrl', ['$scope', '$routeParams',
-       function($scope, $routeParams) {
-       }]);
+vanavaniControllers.controller('mentorsCtrl', [ '$scope', '$routeParams',
+		function($scope, $routeParams) {
+		} ]);
 
-vanavaniControllers.controller('aboutTheSchoolCtrl', ['$scope', '$routeParams',
-                                               function($scope, $routeParams) {
-                                               }]);
+vanavaniControllers.controller('aboutTheSchoolCtrl', [ '$scope',
+		'$routeParams', function($scope, $routeParams) {
+		} ]);
 
-vanavaniControllers.controller('homeCtrl', ['$scope',
-                                                    function($scope) {
-                                                    }]);
+vanavaniControllers.controller('homeCtrl', [ '$scope', function($scope) {
+	$scope.$on('$viewContentLoaded', function() {
+		window.scrollTo(0, 180);		
+		$("#circle0").addClass("circleActive");
+		$("#circle1").addClass("circleActive");
+		$("#circle2").addClass("circleActive");
+		$("#circle3").addClass("circleActive");
+		$("#circle4").addClass("circleActive");
+		$("#circle5").addClass("circleActive");		
+		
+	});
+} ]);
 
-vanavaniControllers.controller('ptaCtrl', ['$scope',
-                                            function($scope) {	
-                                            }]);
+vanavaniControllers.controller('ptaCtrl', [ '$scope', function($scope) {
+} ]);
 
-vanavaniControllers.controller('photoGalleryCtrl', ['$scope',
-                                            function($scope) {
-                                            }]);
+vanavaniControllers.controller('photoGalleryCtrl', [ '$scope',
+		function($scope) {
+		} ]);
 
-vanavaniControllers.controller('videoGalleryCtrl', ['$scope',
-                                            function($scope) {
-                                            }]);
+vanavaniControllers.controller('videoGalleryCtrl', [ '$scope',
+		function($scope) {
+		} ]);
 
-vanavaniControllers.controller('showCaseCtrl', ['$scope',
-                                            function($scope) {
-                                            }]);
+vanavaniControllers.controller('showCaseCtrl', [ '$scope', function($scope) {
+} ]);
 
-vanavaniControllers.controller('contactUsCtrl', ['$scope',
-                                            function($scope) {
-                                            }]);
+vanavaniControllers.controller('contactUsCtrl', [ '$scope', function($scope) {
+} ]);
 
-vanavaniControllers.controller('jobsCtrl', ['$scope',
-                                            function($scope) {
-                                            }]);
-vanavaniControllers.controller('trusteesCtrl', ['$scope',
-                                            function($scope) {
-                                            }]);
-vanavaniControllers.controller('newsLetterCtrl', ['$scope',
-                                            function($scope) {
-                                            }]);
-vanavaniControllers.controller('studentParticularsCtrl', ['$scope',
-                                            function($scope) {
-                                            }]);
-vanavaniControllers.controller('goalsCtrl', ['$scope',
-                                            function($scope) {
-                                            }]);
-vanavaniControllers.controller('howToSponsorCtrl', ['$scope',
-                                            function($scope) {
-                                            }]);
-vanavaniControllers.controller('thankYouCtrl', ['$scope',
-                                            function($scope) {
-                                            }]);
+vanavaniControllers.controller('jobsCtrl', [ '$scope', function($scope) {
+} ]);
+vanavaniControllers.controller('trusteesCtrl', [ '$scope', function($scope) {
+} ]);
+vanavaniControllers.controller('newsLetterCtrl', [ '$scope', function($scope) {
+} ]);
+vanavaniControllers.controller('studentParticularsCtrl', [ '$scope',
+		function($scope) {
+		} ]);
+vanavaniControllers.controller('goalsCtrl', [ '$scope', function($scope) {
+} ]);
+vanavaniControllers.controller('howToSponsorCtrl', [ '$scope',
+		function($scope) {
+		} ]);
+vanavaniControllers.controller('thankYouCtrl', [ '$scope', function($scope) {
+} ]);
